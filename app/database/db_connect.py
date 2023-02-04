7a85035859426c4a3e684695ca2cfc3e7f67cbd5
chanakorn aramsak <solo20113@gmail.com>
2023-02-04 13:52:34 +0000

    change database schema reduce tasklists

--- app/database/db_connect.py
+++ app/database/db_connect.py
@@ -13,16 +13,9 @@ cursor.execute('''
         task_list_id INTEGER NOT NULL,
         description TEXT NOT NULL,
         is_completed INTEGER NOT NULL DEFAULT 0,
-        FOREIGN KEY(task_list_id) REFERENCES task_lists(id)
+        date DATE NOT NULL
     );
-''')
 
-# Execute a CREATE TABLE  for task_lists table
-cursor.execute('''
-    CREATE TABLE task_lists(
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL
-    );
 ''')
 
 
